80551bec9b613fdc7b4702155e6af4940c3ff09e
Fırat Batar <firatbatar@gmail.com>
2026-09-22 13:25:59 +0300

    Frontend initilize

---
+++ sezzle-calculator-frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
+        </main>
+    </div>
+  );
+}
